84ccaf7643e9c34620c39194ada588a439b4a5b0
adavyas <adavyasharma@gmail.com>
2026-06-30 18:12:21 -0400

    Bump native Utopic ref

--- tests/test_package_metadata.py
+++ tests/test_package_metadata.py
@@ -19,7 +19,7 @@ CORE_CATALOG_PATH = (
     / "utopic_core"
     / "models.json"
 )
-EXPECTED_NATIVE_REF = "2483356529460bec0e2b5758bbc1826ed4ae0ad3"
+EXPECTED_NATIVE_REF = "63efe5948d9f707ef87c95a00e772b0248184715"
 
 REQUIRED_NATIVE_RUNNER_FILES = {
     "runner.cpp",
